58cf5d5f1e557c061eee1ed19cd4b2e7318358a4
Kayque Santos <119762234+KayqueSanto@users.noreply.github.com>
2023-09-11 20:26:55 -0300

    Pseudocódigo.py
    11.09.2023

---
+++ Pseudocódigo.py
@@ -0,0 +1,5 @@
+print("Bem-vindo ao calculador de Área de paralelograma")
+base = float(input("Insira o comprimento da base: "))
+altura = float(input("Insira a altura: "))
+area = base * altura
+print("A área do paralelograma é: ", area)
